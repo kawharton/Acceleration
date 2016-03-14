20ed9a6811893a120e301dec6bf1f831a8cebfee
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2016-03-13 20:04:26 -0400

    moving forward with phone acceleration

--- browser/app.js
+++ browser/app.js
@@ -1,15 +1,16 @@
 // we need this socket object to send messages to our server 
 var socket = io(window.location.origin); 
-  
+var moves = 0;
+  var lastMoveTime = 0;
 
 
-  var mover = 0;
-var red = document.getElementById('red');
-function move (input) {
+//   var mover = 0;
+// var red = document.getElementById('red');
+// function move (input) {
 	
-red.style.marginLeft = mover;
-mover+=(input);
-} 
+// red.style.marginLeft = mover;
+// mover+=(input);
+// } 
 
 
 
@@ -25,9 +26,45 @@ socket.on('connect', function(){
 
   socket.on('otherDraw', function(data){
   	//console.log(e.acceleration.y);
-  	if(data) move(Math.abs(data));
-// console.log(e.acceleration.y);
-//     console.log(e);
+    
+  	// if(data) move(Math.abs(data));
+            var d = new Date();
+            var currentTime = d.getTime();
+  //e.key="";    // just enter the char you want to send 
+
+  if(data && Math.abs(data) > 4 && (currentTime-lastMoveTime)>5000) {
+    console.log(currentTime - lastMoveTime);
+    lastMoveTime = currentTime;
+    console.log(data);
+    var e = new Event("keydown");
+  e.keyCode=38;
+  e.which=e.keyCode;
+  e.altKey=false;
+  e.ctrlKey=true;
+  e.shiftKey=false;
+  e.metaKey=false;
+
+  window.dispatchEvent(e);
+  // e.preventDefault();
+  // e.stopImmediatePropagation();
+  
+  // moves++;
+  // console.log(moves);
+setTimeout(function(){
+     var e = new Event("keyup");
+  e.keyCode=38;
+  e.which=e.keyCode;
+  e.altKey=false;
+  e.ctrlKey=true;
+  e.shiftKey=false;
+  e.metaKey=false;
+
+  window.dispatchEvent(e);
+}, 250)
+
+  }
+ 
+
   })
   
 })
