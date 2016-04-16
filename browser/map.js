547f3f99d086bf35c1ac1093206f5f37f441f55c
Khadija Wharton <kwharton128@gmail.com>
2016-04-16 01:11:36 -0700

    maps sort of working

--- browser/map.js
+++ browser/map.js
@@ -1,7 +1,24 @@
 var map;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 42.345573, lng: -71.098326},
+          zoom: 20
         });
       }
+
+// function initialize() {
+//   var fenway = {lat: 42.345573, lng: -71.098326};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: fenway,
+//     zoom: 14
+//   });
+//   var panorama = new google.maps.StreetViewPanorama(
+//       document.getElementById('pano'), {
+//         position: fenway,
+//         pov: {
+//           heading: 34,
+//           pitch: 10
+//         }
+//       });
+//   map.setStreetView(panorama);
+// }
