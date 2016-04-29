6eaf1813dee9a79f40f5f5d85ba073db45290667
Khadija Wharton <khadija@kw.local>
2016-04-28 22:18:02 -0400

    use streetview api to move forward

--- browser/app.js
+++ browser/app.js
@@ -2,7 +2,16 @@
 var socket = io(window.location.origin); 
 var stepSound = new Audio("../step.wav");
 
-document.getElementById('map').focus();
+/*window.addEventListener('keydown', function(){
+  console.log('hi from window'); 
+
+  var e = new Event("keydown");
+  e.keyCode=38;
+  e.which=e.keyCode;
+  document.getElementById('map').dispatchEvent(e);
+});
+
+document.getElementById('map').addEventListener('keydown', function(){console.log('hi from map');}, true);*/
 
 socket.on('connect', function(){
 
@@ -18,11 +27,44 @@ socket.on('connect', function(){
     if(data && Math.abs(data) > 3) {
 
       console.log(data);
-      var e = new Event("keydown");
-      e.keyCode=38;
-      e.which=e.keyCode;
 
-      window.dispatchEvent(e);
+  var e = new KeyboardEvent("keydown");
+
+
+  e.altKey = false;
+e.bubbles = true;
+e.cancelBubble = false
+e.cancelable = true;
+e.charCode = 0;
+e.ctrlKey = false;
+e.currentTarget = document;
+e.defaultPrevented = false;
+e.detail = 0;
+e.eventPhase = 3;
+e.isTrusted = true;
+e.isTrusted = true;
+e.keyCode = 38;
+e.keyIdentifier = "Up";
+e.keyLocation = 0
+//location = 0;
+metaKey = false;
+//path: Array[4]
+e.repeat = false;
+e.returnValue = true;
+e.shiftKey = false;
+//sourceCapabilities = InputDeviceCapabilities;
+e.srcElement = document.body;
+e.target = document.body;
+e.timeStamp = 1461471064837;
+e.type = "keydown";
+//view = Window
+e.which = 38;
+
+  /*e.keyCode=38;
+  e.which=e.keyCode;*/
+
+      document.dispatchEvent(e);
+      moveForward();
 
       var timeout;
       if (data < 4){
@@ -55,7 +97,7 @@ socket.on('connect', function(){
        e.shiftKey=false;
        e.metaKey=false;
 
-       window.dispatchEvent(e);
+       document.dispatchEvent(e);
      }, timeout);
     }
   })
@@ -91,3 +133,32 @@ socket.on('connect', function(){
 // });
 
 
+function difference(link) {
+  return Math.abs(panorama.pov.heading%360 - link.heading);
+}
+
+function moveForward() {
+ // console.log('moveForward');
+//  console.log(panorama);
+
+  var curr;
+  for(i=0; i < panorama.links.length; i++) {
+    console.log('in for loop');
+        console.log(panorama.links[i]);
+
+    var differ = difference(panorama.links[i]);
+    if(curr == undefined) {
+      curr = panorama.links[i];
+    }
+
+    if(difference(curr) > difference(panorama.links[i])) {
+      curr = panorama.links[i];
+
+    }
+  }
+
+  console.log(curr.pano);
+  panorama.setPano(curr.pano);
+  panorama.setPov({heading:curr.heading,pitch:0});
+  panorama.setVisible(true);
+}
--- browser/map.js
+++ browser/map.js
@@ -22,18 +22,18 @@
 //       });
 //   map.setStreetView(panorama);
 // }
-
+var panorama;
 function initPano() {
   // Note: constructed panorama objects have visible: true
   // set by default.
-  var panorama = new google.maps.StreetViewPanorama(
+  panorama = new google.maps.StreetViewPanorama(
       document.getElementById('map'), {
         position: {lat: 42.345573, lng: -71.098326},
         addressControlOptions: {
           position: google.maps.ControlPosition.BOTTOM_CENTER
         },
-        linksControl: false,
-        panControl: false,
-        enableCloseButton: false
+//        linksControl: false,
+        panControl: true
+//        enableCloseButton: false
   });
 }
